5575e26a53ecfcc635ab446c52086423298af603
Lien Vo <nlvo@live.nl>
2019-02-13 23:31:39 +0100

    add locked box exercise

--- l5-the-locked-box.js
+++ l5-the-locked-box.js
@@ -1,1 +1,38 @@
- 
+'use strict';
+
+const box = {
+    locked: true,
+    unlock() {
+        this.locked = false;
+    },
+    lock() {
+        this.locked = true;
+    },
+    _content: [],
+    get content() {
+        if (this.locked) throw new Error("Locked!");
+        return this._content;
+    }
+};
+
+function withBoxUnlocked(body) {
+    if (box.locked == true) {
+        box.unlock();
+        return body();
+    }
+
+    try {
+        return body();
+    } catch (e) {
+        console.log("Error raised:", e);
+    } finally {
+        box.lock();
+    }
+
+}
+
+withBoxUnlocked(function() {
+    box.content.push("gold piece");
+});
+
+console.log(box.locked);
